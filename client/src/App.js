be7a13deb930aa9f38e8eb85d86ae19d3086baa8
Will Lennox <will.mlennox@gmail.com>
2022-01-30 12:51:39 -0500

    Redid the setup for website.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
-import React from "react";import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/navbar";
-import RecordList from "./components/recordList";
-import Edit from "./components/edit";
-import Create from "./components/create";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
-const App = () => {
- return (
-   <div>
-     <Navbar />
-     <Routes>
-       <Route exact path="/" element={<RecordList />} />
-       <Route path="/edit/:id" element={<Edit />} />
-       <Route path="/create" element={<Create />} />
-     </Routes>
-   </div>
- );
-};
+function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+
+  // once hosted change this to actual url
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+    })
+  }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:3001/createUser", {
+      name, 
+      username,
+    }).then((response) => {
+      setListOfUsers([...listOfUsers, {
+      name, 
+      username,
+    }])
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="usersDisplay">
+        {listOfUsers.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Username: {user.username}</h1>
+            </div>
+          );
+        })}
+      </div>
+
+      <div>
+        <input 
+          type="text" 
+          placeholder="Name..." 
+          onChange={(event) => {
+            setName(event.target.value)
+          }}
+        />
+        <input 
+          type="text" 
+          placeholder="setUsername..." 
+          onChange={(event) => {
+            setUsername(event.target.value)
+          }}
+        />
+        <button onClick={createUser}> Create User </button>
+      </div>
+    </div>
+  );
+}
 
 export default App;
