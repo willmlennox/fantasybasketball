c72475d4c183abd5a621c469bcb4db2cfef35f84
Will <will.mlennox@gmail.com>
2022-02-09 19:20:35 -0500

    adding dist subtree

---
+++ dist/js/chunk-2d22d746.5c872d1c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,c){"use strict";c.r(t);var e=c("7a23"),u={class:"about"},a=Object(e["f"])("h1",null,"About",-1),o=[a];function r(n,t){return Object(e["r"])(),Object(e["e"])("main",u,o)}var b=c("6b0d"),s=c.n(b);const d={},i=s()(d,[["render",r]]);t["default"]=i}}]);
+//# sourceMappingURL=chunk-2d22d746.5c872d1c.js.map
